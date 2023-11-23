b91214bf845815c28eeb5800dfb5b6550960cb3f
DaThuzt <117471031+DaThuztdontknowcode@users.noreply.github.com>
2023-11-23 19:48:06 +0700

    Update bt3.js

--- bt3.js
+++ bt3.js
@@ -20,16 +20,16 @@ class Geometry extends Information {
     }
 }
 
-// Tạo đối tượng của lớp Geometry
+
 const geometryObject = new Geometry("Square", "A four-sided polygon with equal sides", 5);
 
-// In ra các thuộc tính của lớp Information
+
 console.log(`Tên: ${geometryObject.name}`);
 console.log(`Mô tả: ${geometryObject.description}`);
 
-// In ra các thuộc tính của lớp Geometry
+
 console.log(`Chiều dài: ${geometryObject.length}`);
 
-// Gọi hàm tính toán từ lớp Information thông qua lớp Geometry
+
 const computedValue = geometryObject.computeGeometryValue(2);
 console.log(`Giá trị tính toán: ${computedValue}`);
